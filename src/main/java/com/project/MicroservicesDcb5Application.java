0e218fdcc70cbab348e2d83ce0e55e3bc9200901
Emerson Millard <emillard.code@gmail.com>
2022-06-19 12:00:00 -0700

    Added Eureka and Hystrix annotations to main class.

--- src/main/java/com/project/MicroservicesDcb5Application.java
+++ src/main/java/com/project/MicroservicesDcb5Application.java
@@ -2,8 +2,13 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableEurekaClient
 public class MicroservicesDcb5Application {
 
     public static void main(String[] args) {
